5128692d1ec63617fe6ee60ccbfbcfce8dce8ce6
Piyush-Kumar-Absent <piyushskumar4613@gmail.com>
2026-05-03 16:22:49 +0530

    Update Assignmentscodes.py

--- Assignmentscodes.py
+++ Assignmentscodes.py
@@ -1,1 +1,8 @@
-
+# Simple Function 
+def check_even_odd(num):
+    if num%2==0 :
+        print("even")
+    else :
+         print("odd")
+         num = int(input("enter a numbewr"))
+         check_even_odd(num)
